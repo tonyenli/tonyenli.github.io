66e688eaabba584f2e4dbf5802db9e49a33e14fa
tonyenli <tonyenli@gmail.com>
2016-12-20 19:02:56 +0800

    dev1 version 12/20/2016

---
+++ class.js
@@ -0,0 +1,23 @@
+student = {
+    name: "tony",
+    phone: "0912345678",
+    address: "tw"
+    moveforward: function (distance) {}
+};
+
+student.name
+
+student.moveforward(100)
+
+
+product = {
+    id: ,
+    name: ,
+    price:
+};
+
+product.id
+product.name
+product.price
+
+document.write(student.name);
